ec03ec9e583c43bd458a2dc6100b92cfdaa22a7b
Jan Heist <heist@rptu.de>
2023-07-26 15:30:36 +0200

    refactoring

--- scripts/rptu-timetools.js
+++ scripts/rptu-timetools.js
@@ -1,4 +1,6 @@
-const prefix = 'timetools-';
+const prefix = 'timetools-'; // Prefix für die Document-ID's
+
+// Template = Ausgabe-Vorlage
 const template = `
 <div>
     <br/>
@@ -30,20 +32,31 @@ const template = `
     </table>
 </div>`;
 
+/**
+ * Führende Nullen hinzufügen
+ */
 function leadingZero(num) {
     return num < 10 ? '0' + num : num;
 }
 
+/**
+ * Date formatieren
+ */
 function formatTime(date) {
     return leadingZero(date.getHours()) + ':' + leadingZero(date.getMinutes());
 }
 
-function calculateNow(start, today) {
+/**
+ * Berechnen der Zeit bis jetzt
+ */
+function calculateNow(startTime, today) {
+    // Aktuelle Zeit und Differenz ermitteln
     let _now = new Date();
-    start = new Date(today + start);
-    _now = _now - start;
+    startTime = new Date(today + startTime);
+    _now = _now - startTime;
     _now = new Date(_now);
 
+    // Pausen berechnen
     if (_now.getHours() == 6) {
         _now.setMinutes(_now.getMinutes() - Math.min(30, _now.getMinutes()));
     } else if (_now.getHours() > 9) {
@@ -51,56 +64,73 @@ function calculateNow(start, today) {
     } else if (_now.getHours() >= 6) {
         _now.setMinutes(_now.getMinutes() - 30);
     }
+
+    // Formatieren und ausgeben
     _now = formatTime(_now);
     document.querySelector('#' + prefix + 'now').textContent = _now;
 }
 
-function calculateTime(start, today) {
-    let _0845 = new Date(today + start);
+/**
+ * Berechnen und ausgeben der Zeiten
+ */
+function calculateTime(startTime, today) {
+    // 08:45
+    let _0845 = new Date(today + startTime);
     _0845.setHours(_0845.getHours() + 8);
     _0845.setMinutes(_0845.getMinutes() + 45);
     _0845 = formatTime(_0845);
 
-    let _0818 = new Date(today + start);
+    // 08:18
+    let _0818 = new Date(today + startTime);
     _0818.setHours(_0818.getHours() + 8);
     _0818.setMinutes(_0818.getMinutes() + 18);
     _0818 = formatTime(_0818);
 
-    let _0600 = new Date(today + start);
+    // 06:00
+    let _0600 = new Date(today + startTime);
     _0600.setHours(_0600.getHours() + 6);
     _0600 = formatTime(_0600);
 
-    calculateNow(start, today);
-
+    // Ermittelte Daten ausgeben
     document.querySelector('#' + prefix + '0845').textContent = _0845;
     document.querySelector('#' + prefix + '0818').textContent = _0818;
     document.querySelector('#' + prefix + '0600').textContent = _0600;
+
+    // Zeit bis jetzt berechnen
+    calculateNow(startTime, today);
 }
 
-const data = document.querySelector('#ContentPlaceHolder1_GVBuchungen_Label1_0');
-if (data) {
-    const parentElem = document.querySelector('#ContentPlaceHolder1_GVBuchungen').parentElement;
+// region main
+const dataTable = document.querySelector('#ContentPlaceHolder1_GVBuchungen_Label1_0');
+if (dataTable) {
+    // Neues Element anlegen
     let templateHTML = document.createElement('div');
     templateHTML.id = prefix + 'calulatedTime';
     templateHTML.innerHTML = template;
+
+    // Neues Element einfügen
+    const parentElem = document.querySelector('#ContentPlaceHolder1_GVBuchungen').parentElement;
     parentElem.append(templateHTML);
 
-    let start = document.querySelector('#ContentPlaceHolder1_GVBuchungen_Label1_0').innerText;
-    if (start) {
+    // Zeit ermitteln
+    let startTime = document.querySelector('#ContentPlaceHolder1_GVBuchungen_Label1_0').innerText;
+    if (startTime) {
+        // Parsen und formatieren heutiger Tag
         let today = new Date();
-        let d = today.getDate();
-        let m = today.getMonth() + 1;
-        let y = today.getFullYear();
-        d = leadingZero(d);
-        m = leadingZero(m);
-        today = m + '/' + d + '/' + y + ' ';
+        let day = leadingZero(today.getDate());
+        let month = leadingZero(today.getMonth() + 1);
+        let year = leadingZero(today.getFullYear());
+        today = month + '/' + day + '/' + year + ' ';
 
-        calculateTime(start, today);
+        // Zeit berechnen
+        calculateTime(startTime, today);
 
+        // Jede Minute aktuelle Zeit aktualisieren
         setInterval(function () {
-            calculateNow(start, today);
+            calculateNow(startTime, today);
         }, 60000);
     }
 }
 
 console.log("%cRPTU Timeserver Addon von Jan Heist", "background: white; color: gray; font-size: x-large; border: 5px solid; border-radius: 3px; padding: 10px; text-align: center");
+// endregion main
